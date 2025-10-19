a33c3d85e98a5cba8bed22a0be588847eba63af4
radeet <greenglass0k@gmail.com>
2025-10-19 11:58:55 +0800

    kode praktikum materi week 6

--- week6_DIctionary_and_string/dictionary.py
+++ week6_DIctionary_and_string/dictionary.py
@@ -1,27 +1,27 @@
 # ===== KODE MODUL PRAKTIKUM MINGGU 6 =======
-# import pprint
-# dictionary_buah = {
-#         "Pisang" : 10000,
-#         "Apel" : 5000,
-#         "Mangga" : 4000,
-#         "Jambu" : 12000
-# }
-# # Mengeprint semua dictionary
-# print("\nIni adalah contoh dictionary, berikut adalah dictionary yang sudah kita buat")
-# print(dictionary_buah)
+import pprint
+dictionary_buah = {
+        "Pisang" : 10000,
+        "Apel" : 5000,
+        "Mangga" : 4000,
+        "Jambu" : 12000
+}
+# Mengeprint semua dictionary
+print("\nIni adalah contoh dictionary, berikut adalah dictionary yang sudah kita buat")
+print(dictionary_buah)
 
-# # Mengerprint key nya saja
-# print("\nIni jika kita hanya ingin menge-print key dictionary nya:")
-# print(dictionary_buah.keys())
-# # menggunakan pprint
+# Mengerprint key nya saja
+print("\nIni jika kita hanya ingin menge-print key dictionary nya:")
+print(dictionary_buah.keys())
+# menggunakan pprint
 
-# print("\nIni jika kita menggunakan library pprint: ")
-# pprint.pprint(dictionary_buah)
+print("\nIni jika kita menggunakan library pprint: ")
+pprint.pprint(dictionary_buah)
 
-# b = {1: 'Januari', 2: 'Februari', 'Januari' : 1, 'Februari' : 2} 
-# print(b[2]) 
-# print(b['Januari']) 
-# print(b) 
+b = {1: 'Januari', 2: 'Februari', 'Januari' : 1, 'Februari' : 2} 
+print(b[2]) 
+print(b['Januari']) 
+print(b) 
 
 
 # ===== TOKO BUAH ======
@@ -63,16 +63,18 @@ while (choice == 'y') or (choice == 'Y') :
 print("baiklah selamat tinggal")
 
 
-# # ===== EXTRA =====
-# def print_game_item_list(data) :
-#     print("Daftar Items: ")
-#     for item, amount in data.items() :
-#         print("{} ({})").format(item, amount)
+# ===== EXTRA =====
+def print_game_item_list(data) :
+    print("Daftar Items: ")
+    for item, amount in data.items() :
+        print("{} ({})".format(item, amount))
 
 
 
-# inventory = {
-#         "Sekop" : 4,
-#         "Medical Kit" : 3,
-#         "Granat" : 2
-#     }
+inventory = {
+        "Sekop" : 4,
+        "Medical Kit" : 3,
+        "Granat" : 2
+    }
+
+print_game_item_list(inventory)
